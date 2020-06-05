d937927de2160105a8572b01ffebd6f3b9b12b90
negibose2020 <allium.bose@gmail.com>
2020-06-05 22:24:28 +0900

    AC

--- ABC157_C.py
+++ ABC157_C.py
@@ -1,24 +1,31 @@
 N,M=map(int,input().split())
-ans=['A']*(N+1)
+ans=[-1]*(N+1)
 for i in range(M):
-    s,c=map(str,input().split())
-    s=int(s)
-    if ans[s]=='A' or ans[s]==c :
+    s,c=map(int,input().split())
+    if ans[s]==-1 :
         ans[s]=c
+    elif ans[s]==c :
+        pass
     else:
         print('-1')
         exit()
-if N>1 and ans[1]=='A':
+
+if N>=2 and ans[1]==0:
     print('-1')
     exit()
-elif N>1 and ans[1]=='0':
-    print('-1')
-    exit()
+elif N>=2 and ans[1]==-1: #ここが間違ってた。
+    ans[1]=1
 else:
     pass
-FA=ans[1:]
-FA=''.join(FA)
-if FA.count('A')>0:
-    print(int(FA.replace('A','0')))
-    exit()
-print(int(FA))
+
+for j in range(len(ans)):
+    if ans[j] == -1:
+        ans[j] = 0
+
+if N==1:
+    FA=ans[-1]
+elif N==2:
+    FA=10*ans[-2]+ans[-1]
+else:
+    FA=100*ans[-3]+10*ans[-2]+ans[-1]
+print(FA)
