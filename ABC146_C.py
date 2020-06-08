a9e4d1001a4f837cc51c930f7e62e2fb5140b521
negibose2020 <allium.bose@gmail.com>
2020-06-08 23:28:59 +0900

    AC

--- ABC146_C.py
+++ ABC146_C.py
@@ -2,25 +2,29 @@ import math
 
 A,B,X=map(int,input().split())
 
-
-N=0
-arr=[X,10**9]
-
 if X<A+B:
-    print(N)
+    print(0)
     exit()
 if X>=10**9*A+9*B:
     print(10**9)
     exit()
 
-if A>=B:
-    for i in range(1,10):
-        _=int((X-i*B)/A)
-        arr.append(_)
+min=0
+max=10**9+1
+N=(min+max)//2
+D=int(math.log10(N)+1)
+
+if A*N+B*D<X
+    N=N/2
 else:
-    for i in range(1,10):
-        _=int((X/i)/A)
-        arr.append(_)
+    N=N+N/2
+
+
+
+
+
+
+
 
 N=1
 arr2=sorted(arr)
@@ -32,9 +36,18 @@ else:
 Ns=sorted([N_from,N_to])
 
 N=Ns[0]
-D=int(math.log10(N)+1)
+print(N)
+print(A*N+B*D1>X)
 
+if A*N+B*D1>X:
+    flg=A*N+B*D1>X
+    D2=int(math.log10(N-1)+1)
+    while flg==True:
+        N-=1
+else:
+    flg==A*N+B*D1<=X
+    D2=int(math.log10(N+1)+1)
+    while flg==True:
+        N+=1
 
-while A*N+B*D<=X:
-    N+=1
 print(N)
