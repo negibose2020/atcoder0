b63475468e8a31911575232a6a827308329e1ad5
negibose2020 <allium.bose@gmail.com>
2020-06-09 10:17:12 +0900

    AC 余計な条件式を入れていたため

--- ABC161_C.py
+++ ABC161_C.py
@@ -1,11 +1,7 @@
-N,K=map(int,input().split())
+N, K = map(int,input().split())
 
-if K>N or N==0:
-    print(N)
-    exit()
+D1 = N//K
+D2 = N//K+1
 
-D1=N//K
-D2=N//K+1
-
-Ans=min (abs(D1*K-N),abs(D2*K-N))
+Ans = min(abs(N-D1*K) ,abs(N-D2*K))
 print(Ans)
