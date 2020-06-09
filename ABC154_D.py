ad425e41edb5c820161c227680fdbfe6e04f5436
negibose2020 <allium.bose@gmail.com>
2020-06-10 08:38:00 +0900

    初めから期待値で配列作成するもTLE

--- ABC154_D.py
+++ ABC154_D.py
@@ -1,28 +1,22 @@
 N,K=map(int,input().split())
-S=list(map(int,input().split()))
+# S=list(map(int,input().split()))
+_=input().split()
+S=[]
+for s in _:
+    S.append((int(s)+1)/2) #初めから期待値で配列作成
 
 # K=4
-# S=(17, 13, 13, 12, 15, 20, 10, 13, 17, 11)
+# S=[17, 13, 13, 12, 15, 20, 10, 13, 17, 11]
 # K=3
 # S=[1,2,2,4,5]
 
-# K個連続した区間での最大が期待値の最大として、SでK個連続した最大値を探す。
-maxSKsum=0
-startSindex=0
+maxsum=0
 
-#1.大きい区間を探す
 for i in range(len(S)):  #ここがrange(len(S)-K)ではダメっぽいのがわからない・・・
-    tempSKsum=sum(S[i:i+K])
-    if maxSKsum<tempSKsum:
-        maxSKsum=tempSKsum
-        startSindex=i
+    tempsum=sum(S[i:i+K])
+    if maxsum<tempsum:
+        maxsum=tempsum
     else:
         pass
 
-#2.1で探した大きい区間の計算をする。
-EV=0
-for s in range(K):
-    tempEV=S[startSindex+s]*(S[startSindex+s]+1)/2/S[startSindex+s]
-    EV+=tempEV
-
-print(EV)
+print(maxsum)
