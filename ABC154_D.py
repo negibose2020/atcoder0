580acd2f2834049d5633e02e5476a474acc0c7c0
negibose2020 <allium.bose@gmail.com>
2020-06-09 23:56:15 +0900

    1つTLE減ったけどTLE...

--- ABC154_D.py
+++ ABC154_D.py
@@ -6,10 +6,12 @@ S=list(map(int,input().split()))
 # K=3
 # S=[1,2,2,4,5]
 
+# K個連続した区間での最大が期待値の最大として、SでK個連続した最大値を探す。
 maxSKsum=0
 startSindex=0
 
-for i in range(len(S)):  #ここがrange(len(S)-K)ではダメないのがわからない・・・
+#1.大きい区間を探す
+for i in range(len(S)):  #ここがrange(len(S)-K)ではダメっぽいのがわからない・・・
     tempSKsum=sum(S[i:i+K])
     if maxSKsum<tempSKsum:
         maxSKsum=tempSKsum
@@ -17,9 +19,10 @@ for i in range(len(S)):  #ここがrange(len(S)-K)ではダメないのがわか
     else:
         pass
 
-EVlist=[]
+#2.1で探した大きい区間の計算をする。
+EV=0
 for s in range(K):
     tempEV=S[startSindex+s]*(S[startSindex+s]+1)/2/S[startSindex+s]
-    EVlist.append(tempEV)
+    EV+=tempEV
 
-print(sum(EVlist))
+print(EV)
