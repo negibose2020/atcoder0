9e571d7bec0cba09be98d0a358241aa4602bc5da
negibose2020 <allium.bose@gmail.com>
2020-06-15 23:59:04 +0900

    ブログ用リファクタリング

--- ABC170_C.py
+++ ABC170_C.py
@@ -1,37 +1,28 @@
-X,N=map(int,input().split())
-if N==0:
+X, N = map(int,input().split())
+if N == 0:
     print(X)
     exit()
 
-P_ori=list(map(int,input().split()))
-if P_ori.count(X)==0:
+p = list(map(int,input().split()))
+if p.count(X) == 0:
     print(X)
     exit()
 
 
-P1=sorted(P_ori)
-N=list(range(0,102))
-PZ=[]
-for i in range(len(N)):
-    if P1.count(N[i])==0:
-        PZ.append(N[i])
+l = list(range(0,102)) #ここの範囲設定が一番難しい部分だと思う
+flist = []
+for i in range(len(l)):
+    if p.count(l[i]) == 0:
+        flist.append(l[i])
 
-PZ.append(X)
-P2=sorted(PZ)
-t=P2.index(X)
-if P2[t]==P2[0]:
-    print(P2[1])
-    exit()
-elif P2[t]==P2[-1]:
-    print(P2[-2])
-    exit()
+
+flist.append(X)
+anslist = sorted(flist)
+t = anslist.index(X)
+
+dist1 = anslist[t] - anslist[t-1] #Xと1つ前の数字との距離
+dist2 = anslist[t+1] - anslist[t] #Xと1つ後ろの数字との距離
+if dist1 <= dist2:
+    print(anslist[t-1])
 else:
-    dist1=abs(P2[t-1]-P2[t])
-    dist2=abs(P2[t]-P2[t+1])
-    if dist1<=dist2:
-        print(P2[t-1])
-        exit()
-    else:
-        print(P2[t+1])
-        exit()
-print(0)
+    print(anslist[t+1])
