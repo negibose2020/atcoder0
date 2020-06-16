fd42d25e666947c9d20c4d1e2db6157775e9e15a
negibose2020 <allium.bose@gmail.com>
2020-06-16 11:36:28 +0900

    コメントと変数を少し修正_AC

--- ABC170_C.py
+++ ABC170_C.py
@@ -3,25 +3,29 @@ if N == 0:
     print(X)
     exit()
 
-p = list(map(int,input().split()))
-if p.count(X) == 0:
+# titleの通り、使用を禁止された数字のリストが与えられる。
+flist = list(map(int,input().split()))
+
+if flist.count(X) == 0:
     print(X)
     exit()
 
+# 使用禁止の数字を除いたリストを作るために、リストの素を作る。
+l = list(range(0,102)) # ここの範囲設定が一番難しい部分だと思う。
 
-l = list(range(0,102)) #ここの範囲設定が一番難しい部分だと思う
-flist = []
+# 使用を許可された数字のリストを作成する。
+plist = []
 for i in range(len(l)):
-    if p.count(l[i]) == 0:
-        flist.append(l[i])
+    if flist.count(l[i]) == 0:
+        plist.append(l[i])
 
-
-flist.append(X)
-anslist = sorted(flist)
+# Xを投入して、前後の数字との距離を計測していく。
+plist.append(X)
+anslist = sorted(plist)
 t = anslist.index(X)
 
-dist1 = anslist[t] - anslist[t-1] #Xと1つ前の数字との距離
-dist2 = anslist[t+1] - anslist[t] #Xと1つ後ろの数字との距離
+dist1 = anslist[t] - anslist[t-1] # Xと1つ前の数字との距離
+dist2 = anslist[t+1] - anslist[t] # Xと1つ後ろの数字との距離
 if dist1 <= dist2:
     print(anslist[t-1])
 else:
