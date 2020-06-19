c521091af851c52748fb57d4a77c093bef7c81d3
negibose2020 <allium.bose@gmail.com>
2020-06-19 12:59:43 +0900

    もうちょっとでいい感じになりそうなので一応コミット

--- ABC146_C.py
+++ ABC146_C.py
@@ -1,53 +1,37 @@
-import math
+def CanIBuy(A,B,mid):
+    D=len(str(mid))
+    price=A*mid+B*D
+    return price
+
 
 A,B,X=map(int,input().split())
 
-if X<A+B:
-    print(0)
-    exit()
-if X>=10**9*A+9*B:
-    print(10**9)
-    exit()
+left=0
+right=10**9
 
-min=0
-max=10**9+1
-N=(min+max)//2
-D=int(math.log10(N)+1)
+for i in range(X):
 
-if A*N+B*D<X
-    N=N/2
-else:
-    N=N+N/2
+    if CanIBuy(A,B,(left+right)//2)<X:
+        _left=left
+        _right=right
+        '''
+        このifの部分は怪しい
+        '''
+        if left==(_left+_right)//2:
+            left+=1
+        else:
+            left=(_left+_right)//2
+    elif CanIBuy(A,B,(left+right)//2)>X:
+        _left=left
+        _right=right
+        '''
+        このifの部分は怪しい
+        '''
+        if right==(_left+_right)//2:
+            right-=1
+        right=(_left+_right)//2
+    else:
+        break
+    print(i,left,right)
 
-
-
-
-
-
-
-
-N=1
-arr2=sorted(arr)
-N_from=arr2[arr2.index(X)-1]
-if arr2[-1]==X:
-    N_to=X
-else:
-    N_to=arr2[arr2.index(X)+1]
-Ns=sorted([N_from,N_to])
-
-N=Ns[0]
-print(N)
-print(A*N+B*D1>X)
-
-if A*N+B*D1>X:
-    flg=A*N+B*D1>X
-    D2=int(math.log10(N-1)+1)
-    while flg==True:
-        N-=1
-else:
-    flg==A*N+B*D1<=X
-    D2=int(math.log10(N+1)+1)
-    while flg==True:
-        N+=1
-
-print(N)
+print(left)
