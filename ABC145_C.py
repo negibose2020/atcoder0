630e3c37aef9691d6fc3b973373fdc6f0be4ea48
negibose2020 <allium.bose@gmail.com>
2020-06-26 11:43:19 +0900

    sample1は通った。Nが変更になった際に対応が必要そう(line18あたり)

---
+++ ABC145_C.py
@@ -0,0 +1,31 @@
+import itertools
+def Cal_Dist(i,j):
+    a=((i[0]-j[0])**2+(i[1]-j[1])**2)**0.5
+    return a
+
+N=int(input())
+
+town=[[0]*2 for i in range(N)]
+for i in range (N):
+    xy=list(map(int,input().split()))
+    town[i]=xy
+R=list(itertools.permutations(range(N)))
+
+distance=[]
+
+
+for k in range(len(R)):
+    a,b,c=R[k]
+    temp_town=[town[a],town[b],town[c]]
+    
+    temp_distance=[]
+    for i in range (N-1):
+        distance_sections=Cal_Dist(temp_town[i],temp_town[i+1])
+        temp_distance.append(distance_sections)
+    else:
+        distance.append(sum(temp_distance))
+        # print(a,b,c,temp_distance)
+
+# print(town)
+# print(R)
+print(sum(distance)/len(R))
