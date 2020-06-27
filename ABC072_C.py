67a8ddd5306ac69f49aa19008291f4cae85cba2e
negibose2020 <allium.bose@gmail.com>
2020-06-27 19:29:41 +0900

    3つのケースでWA

--- ABC072_C.py
+++ ABC072_C.py
@@ -3,20 +3,26 @@ a=list(map(int,input().split()))
 
 l=sorted(a)
 
-ans=1
+# N=10
+# l=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+ans=0
 
 for i in range(N):
-    t=l[i]
-    _ans=0
-    for j in range (N-i):
-        c=l[i+j]
-        if c-t<=2:
-            pass
-        else:
-            _ans=j
-            break
-    if _ans>ans:
-        ans=_ans
+    if i>=1 and l[i]==l[i-1]:
+        continue
     else:
-        pass
+        t=l[i]
+        for j in range(N-i):
+            c=l[i+j]
+            if abs(c-t)<=2:
+                continue
+            else:
+                _ans=j
+                if _ans>ans:
+                    ans=_ans
+                else:
+                    pass
+                break
 print(ans)
