33c11279309f36e4a727ad47fba6c8f982bf8c75
negibose2020 <allium.bose@gmail.com>
2020-06-29 18:49:36 +0900

    AC

--- ABC172_C.py
+++ ABC172_C.py
@@ -1,42 +1,41 @@
-import numpy as np
+import bisect
 
-N,M,K=map(int,input().split())
-a=list(map(int,input().split()))
-b=list(map(int,input().split()))
+sysin=sys.stdin.readline
+
+N,M,K=map(int,inp().split())
+a=list(map(int,inp().split()))
+b=list(map(int,inp().split()))
 
 # print(K)
 # print(a)
 # print(b)
 # N,M=3,4
-# K=730
+# K=240
 # a=[60, 90, 120]
 # b=[80, 150, 80, 150]
 
 ans=0
 
-accum_a=np.cumsum(a)
-accum_a=accum_a[accum_a <K]
-accum_a=np.append(accum_a,K)
-accum_a=np.insert(accum_a,0,0)
+accum_a=[0]
+for i in a:
+    _a=accum_a[-1]+i
+    if _a>K:
+        break
+    else:
+        accum_a.append(_a)
+accum_a.append(K)
 
-accum_b=np.cumsum(b)
+accum_b=[0]
+for i in b:
+    accum_b.append(accum_b[-1]+i)
 
 ans=0
-# print(accum_a)
-# print(accum_b)
-# print(len(accum_a))
-
 
 for j in range (len(accum_a)-1):
     Kb=K-accum_a[j]
-    _accum_b=accum_b
-    _accum_b=np.append(_accum_b,Kb)
-    _accum_b1=np.sort(_accum_b)
-    bookb=np.where(_accum_b1==Kb)
-    bookb=np.amax(bookb)
-
-    if j+bookb>ans:
-        ans=j+bookb
+    bookB=bisect.bisect_right(accum_b,Kb)-1
+    if j+bookB>ans:
+        ans=j+bookB
     else:
         pass
 
