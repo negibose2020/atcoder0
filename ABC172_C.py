16f452cb944b947737d4bdc980da6964dc715fd8
negibose2020 <allium.bose@gmail.com>
2020-06-29 13:52:25 +0900

    Aの山を走査して探索する方向にするもTLE

--- ABC172_C.py
+++ ABC172_C.py
@@ -6,32 +6,37 @@ b=list(map(int,input().split()))
 # print(a)
 # print(b)
 # N,M=3,4
-# K=240
+# K=730
 # a=[60, 90, 120]
 # b=[80, 150, 80, 150]
 
 ans=0
-inf=K*2
 
-for i in range (N+M+10):
-    if len(a)==1:
-        a.append(inf)
-    if len(b)==1:
-        b.append(inf)
-    A=a[0]
-    B=b[0]
-    if min(A,B)<=K:
-        ans+=1
-        K-=min(A,B)
-        if A<B:
-            a.pop(0)
-        elif A>B:
-            b.pop(0)
-        else:
-            if A+a[1]<=B+b[1]:
-                a.pop(0)
-            else:
-                b.pop(0)
-    else:
+accum_a=[0]
+for i in a:
+    _a=accum_a[-1]+i
+    if _a>K:
         break
+    else:
+        accum_a.append(_a)
+accum_a.append(K)
+
+accum_b=[0]
+for i in b:
+    accum_b.append(accum_b[-1]+i)
+
+ans=0
+
+for j in range (len(accum_a)-1):
+    Kb=K-accum_a[j]
+    _accum_b=accum_b
+    _accum_b.append(Kb)
+    _accum_b1=sorted(_accum_b)
+    bookb=_accum_b1.index(Kb)-1
+    if _accum_b.count(Kb)==2:
+        bookb+=1
+    if j+bookb>ans:
+        ans=j+bookb
+    else:
+        continue
 print(ans)
