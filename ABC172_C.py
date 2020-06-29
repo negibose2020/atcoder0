2a8a5d5854ef83edccc69b14ad6554c5207218a2
negibose2020 <allium.bose@gmail.com>
2020-06-29 14:49:48 +0900

    numpy使うもWA&TLE

--- ABC172_C.py
+++ ABC172_C.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 N,M,K=map(int,input().split())
 a=list(map(int,input().split()))
 b=list(map(int,input().split()))
@@ -12,31 +14,30 @@ b=list(map(int,input().split()))
 
 ans=0
 
-accum_a=[0]
-for i in a:
-    _a=accum_a[-1]+i
-    if _a>K:
-        break
-    else:
-        accum_a.append(_a)
-accum_a.append(K)
+accum_a=np.cumsum(a)
+accum_a=accum_a[accum_a <K]
+accum_a=np.append(accum_a,K)
+accum_a=np.insert(accum_a,0,0)
 
-accum_b=[0]
-for i in b:
-    accum_b.append(accum_b[-1]+i)
+accum_b=np.cumsum(b)
 
 ans=0
+# print(accum_a)
+# print(accum_b)
+# print(len(accum_a))
+
 
 for j in range (len(accum_a)-1):
     Kb=K-accum_a[j]
     _accum_b=accum_b
-    _accum_b.append(Kb)
-    _accum_b1=sorted(_accum_b)
-    bookb=_accum_b1.index(Kb)-1
-    if _accum_b.count(Kb)==2:
-        bookb+=1
+    _accum_b=np.append(_accum_b,Kb)
+    _accum_b1=np.sort(_accum_b)
+    bookb=np.where(_accum_b1==Kb)
+    bookb=np.amax(bookb)
+
     if j+bookb>ans:
         ans=j+bookb
     else:
-        continue
+        pass
+
 print(ans)
