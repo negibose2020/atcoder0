79ac00d5227e3706ac08dc428eac8a6b51e00db4
negibose2020 <allium.bose@gmail.com>
2020-07-05 01:25:01 +0900

    AC リファクタリング済

--- ABC159_D.py
+++ ABC159_D.py
@@ -2,46 +2,50 @@ import numpy as np
 
 N=int(input())
 A=np.array(list(map(int,input().split())))
+
 '''
 以下、方針
 ・Aから、各要素の出現回数を取得する。
 ・出現回数から、組み合わせを計算する。
 '''
-uni,cou=np.unique(A,return_counts=True)
+unique_num, counts =np.unique(A,return_counts=True) #Aから、各要素の出現回数を取得する。
 
-# print(uni)
-# print(cou)
-dic0={}
-dic1={}
-dic={} #回答用
+# print(unique_num)
+# print(counts)
+dic0={} #数字と出現回数の組合わせ辞書
+dic1={} #数字と組合わせ個数の組合わせ辞書
+dic2={} #回答用の辞書
 
-com=[]
-for i in range(len(cou)):
-    c=cou[i]*(cou[i]-1)//2
-    dic0[uni[i]]=cou[i]
-    dic1[uni[i]]=c
-    com.append(c)
-    dic.setdefault(uni[i])
+Combination_pattern = []
+for i in range(len(counts)):
+    Combination = counts[i] * (counts[i]-1) // 2 #出現回数から、組み合わせを計算する。
+    dic0[unique_num[i]]=counts[i]
+    dic1[unique_num[i]]=Combination
+    dic2.setdefault(unique_num[i])
+    Combination_pattern.append(Combination)
 
-# print(com)
-totalcombination=sum(com)
+totalcombination=sum(Combination_pattern)
 
-# print(com)
+# print(dic0)
 # print(dic1)
+# print(dic2)
+# print(Combination_pattern)
 
 '''
 以下方針
 ・Aから数字を取得
-・その数字に対応する組み合わせを減らす。
-・その数字の出現回数を1減らした状態の組み合わせの数を足す。
+・その数字の出現回数を1減らした状態の組み合わせの数を調べる。
+・その数字に対応する元の組み合わせの数を減らし、1個前で計算した組合わせを足す。
+・同じ数字を引いたときように答えを辞書に登録しておく。
+・もし、過去に同じ数字を引いていたら、登録しておいた答えを回答する。
 '''
 
 for j in range (N):
-    n=A[j] #nはAの数列の数字
-    if dic[n]==None:
-        e=(dic0[n]-1)*(dic0[n]-2)//2
-        t=totalcombination-dic1[n]+e
-        dic[n]=t
-        print(t)
+    n=A[j] #Aから数字を取得
+    if dic2[n]==None:
+        reduced_pattern = (dic0[n]-1) * (dic0[n]-2) // 2 #その数字の出現回数を1減らした状態の組み合わせの数を調べる。
+        ans=totalcombination-dic1[n]+reduced_pattern #その数字に対応する元の組み合わせの数を減らし、1個前で計算した組合わせを足す。
+        dic2[n] = ans #同じ数字を引いたときように答えを辞書に登録しておく。
+        print(ans)
     else:
-        print(dic[n])
+        print(dic2[n]) #もし、過去に同じ数字を引いていたら、登録しておいた答えを回答する。
