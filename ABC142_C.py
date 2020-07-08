e0a52d69afc61c0aa84382cb8fa51fbc0f323933
negibose2020 <allium.bose@gmail.com>
2020-07-08 18:54:55 +0900

    AC 教えてもらってAC(index()は線形探索

--- ABC142_C.py
+++ ABC142_C.py
@@ -1,13 +1,16 @@
 N=int(input())
 Ai=list(map(int,input().split()))
 
-dic={}
+# ans=[]
+ans=[0]*N
 
-for i in range(1,N+1):
-    a=Ai.index(i)+1
-    dic[i]=a
+# for i in range(1,N+1):
+    # a=Ai.index(i)+1
+    # ans.append(str(a))
+for i in range (N):
+    ans[Ai[i]-1]=str(i+1)
 
-ans=''
-for i in range(1,N+1):
-    ans+=str(dic[i])+' '
-print(ans)
+print(' '.join(ans))
+
+# index()は線形探索
+# 逆順列
